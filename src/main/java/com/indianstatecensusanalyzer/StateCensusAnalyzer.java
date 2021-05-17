9ce33de4e906491d0cb77d00b86284506f84f896
Naman <cool.naman.ajmera@gmail.com>
2021-05-17 08:36:00 +0530

    Added: Welcome message.

---
+++ src/main/java/com/indianstatecensusanalyzer/StateCensusAnalyzer.java
@@ -0,0 +1,7 @@
+package com.indianstatecensusanalyzer;
+
+public class StateCensusAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyzer Program");
+    }
+}
