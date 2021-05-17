f1fdc9b69649da2c12e49f44c78725a67385ef31
Naman <cool.naman.ajmera@gmail.com>
2021-05-17 08:57:30 +0530

    2nd Test case passed.

--- src/test/java/com/indianstatecensusanalyzer/SateCensusAnalyzerTest.java
+++ src/test/java/com/indianstatecensusanalyzer/SateCensusAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.indianstatecensusanalyzer;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,8 @@ import java.nio.file.Paths;
 
 public class SateCensusAnalyzerTest {
     private static final String STATE_CENSUS_CSV_FILE_PATH = "E:\\Ebook\\BridgeLabz\\Assignment\\Indian States Census Analyzer\\StateCensusCSV.csv";
+    private static final String WRONG_CSV_FILE_PATH="E:\\Ebook\\BridgeLabz\\Assignment\\Indian States Census Analyzer\\src\\StateCensusCSV.csv";
+
 
     @Test
     public void givenStateCensusCSVFile_ShouldReturnNumberOfRecords() {
@@ -16,6 +19,16 @@ public class SateCensusAnalyzerTest {
             Assertions.assertEquals(5, noOfEntries);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenStateCensusCSVFile_WhenPathIncorrect_ShouldThrowException() {
+        try{
+            StateCensusAnalyzer censusAnalyzer = new StateCensusAnalyzer(Paths.get(WRONG_CSV_FILE_PATH));
+            censusAnalyzer.readStateCensusCSVData();
+        }catch(StateCensusAnalyzerException e) {
+            Assertions.assertEquals(StateCensusAnalyzerException.ExceptionType.INCORRECT_PATH, e.type);
         }
     }
 }
